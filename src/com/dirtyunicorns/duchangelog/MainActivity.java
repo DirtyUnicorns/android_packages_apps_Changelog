21f009dbd4bd9aae91a7f0d1ff707947dc5ad8db
Alex Cruz <mazdarider23@gmail.com>
2016-04-30 07:42:11 -0400

    Redesign of the DU changelog app
    - No longer have to update this app unless is absolutely necessary. The
    changelog is pulled from our server and can/will be updated from there.
    
    The user will need to have data/WiFi on to see the changelog, otherwise
    met with a toast (will replace with a dialog soon) notifying them of such.
    
    Change-Id: I811b27ff5bc86a09db1f5adf91ce325e23316a14

---
+++ src/com/dirtyunicorns/duchangelog/MainActivity.java
@@ -0,0 +1,28 @@
+package com.dirtyunicorns.duchangelog;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+}
