1e8eb7e1ae359d33e7c26feb988858a6c93335f9
Alex Cruz <mazdarider23@gmail.com>
2016-05-03 06:55:42 -0400

    Changelog improvements [2/2]
    Change-Id: Iff9b48cf2f4b738ee470c8671ce1eec59ca8929d

--- src/com/dirtyunicorns/duchangelog/MainActivity.java
+++ src/com/dirtyunicorns/duchangelog/MainActivity.java
@@ -1,9 +1,12 @@
 package com.dirtyunicorns.duchangelog;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +15,30 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Weeklies();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Official();
+            }
+        });
+    }
+
+    public void Weeklies() {
+        Intent w = new Intent(MainActivity.this, Weeklies.class);
+        startActivity(w);
+    }
+
+    public void Official() {
+        Intent o = new Intent(MainActivity.this, Official.class);
+        startActivity(o);
     }
 
     @Override
---
+++ src/com/dirtyunicorns/duchangelog/Weeklies.java
@@ -0,0 +1,31 @@
+package com.dirtyunicorns.duchangelog;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class Weeklies extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weeklies);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+}
+
