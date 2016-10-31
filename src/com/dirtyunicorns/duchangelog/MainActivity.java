398e420584e5033071c379bfbcc54a32025f8179
Alex Cruz <mazdarider23@gmail.com>
2016-10-31 11:46:13 +0100

    Fix build

--- src/com/dirtyunicorns/duchangelog/MainActivity.java
+++ src/com/dirtyunicorns/duchangelog/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.settings.R;
+
 public class MainActivity extends Activity {
 
     @Override
--- src/com/dirtyunicorns/duchangelog/Weeklies.java
+++ src/com/dirtyunicorns/duchangelog/Weeklies.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.settings.R;
+
 public class Weeklies extends Activity {
 
     @Override
